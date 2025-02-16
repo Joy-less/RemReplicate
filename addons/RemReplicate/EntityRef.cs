99cfa8c26a96a7858a5635387d70d9df23d96c22
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-02-16 17:31:07 +0000

    Change EntityRef to struct

--- addons/RemReplicate/EntityRef.cs
+++ addons/RemReplicate/EntityRef.cs
@@ -6,7 +6,7 @@ using MemoryPack;
 namespace RemReplicate;
 
 [MemoryPackable]
-public partial record EntityRef(string Class, Guid Id) {
+public readonly partial record struct EntityRef(string Class, Guid Id) {
     public string Class { get; } = Class;
     public Guid Id { get; } = Id;
 }
